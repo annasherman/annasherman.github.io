95d5d08b97b6a4917ff83b42e71cf4346e130369
Anna Sherman <sherman.anna@gmail.com>
2015-11-10 11:49:42 -0600

    board shows up

--- project1/scripts/app.js
+++ project1/scripts/app.js
@@ -1,3 +1,5 @@
+console.log("Linked.");
+
 // function card(id,text) {
 //   this.id=id,
 //   this.text = text,
@@ -8,20 +10,46 @@
 // }
 
 //randomize how many different options there are
-var options = boardSize*boardSize/2
+
 var liftOptions = ['snatch', 'clean and jerk', 'deadlift', 'bench', 'curl', 'leg press', 'calf raise', 'squat', 'pull up', 'muscle up', 'handstand', 'backbend', 'row', 'lat pulldown', 'decline twist', 'chest fly', 'military press', 'overhead press'];
 
+var boardSize = 6; //rows*columns
 console.log(liftOptions.length);
 
-var aCard = $('<div>');
-aCard.prop('class', 'gamecard');
-$('.gameboard').append(aCard);
+$(document).ready(function(){
 
+var numberOfPairs = boardSize*boardSize/2
 
 //generate cards
-var boardSize = 6;
-var cards = {};
-// for (var i = 0; i < (boardSize*boardSize); i++) {
-//   cards[i] = new card();
-//   $('#gameboard').append(cards[i]);
-// }
+var cards = []; //array holding all the cards
+
+for (var i = 0; i < (boardSize*boardSize); i++) { //make deck
+  cards[i] = {};
+}
+for (var i = 0; i < numberOfPairs; i++) { //assign from array of lifts -- two of each lift.
+  cards[i] = liftOptions[i];
+  cards[i+numberOfPairs] = liftOptions[i];
+}
+
+// shuffleCards(cards);//shuffle cards
+
+for (var i in cards) { //make shit show up
+  var eachCard = $('<div>')
+  eachCard.addClass('cards');
+  eachCard.text(cards[i]);
+  $('.gameboard').append(eachCard);
+}
+
+//shuffle function
+// function shuffleCards(array){
+
+
+//begin game: hide all cards
+
+//onclick: show contents. animation???
+
+console.log(cards);
+
+
+
+}); //end document ready.
